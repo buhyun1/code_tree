d2c910ae02eeedea48bff38c29a1a3cc4a722200
buhyun <102472949+buhyun1@users.noreply.github.com>
2025-01-11 19:25:47 +0900

    Title: 원소 값들의 최대 합, Time: 46ms, Memory: 17MB, Status: Passed - CodeTree

--- 250111/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 250111/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -1,17 +1,21 @@
 n, m = map(int, input().split())
 box = list(map(int, input().split()))
 
-cur_pos = 0
-total = 0
+max_total = 0
 
-for _ in range(m):
-    total += box[cur_pos]
-    next_pos = box[cur_pos]-1
+for start_pos in range(n):
+    cur_pos = start_pos
+    total = 0
 
-    if next_pos <0 and next_pos >= n:
-        break
-    
-    cur_pos = next_pos
+    for _ in range(m):
+        total += box[cur_pos]
+        next_pos = box[cur_pos]-1
 
-print(total)
+        if next_pos <0 and next_pos >= n:
+            break
+        
+        cur_pos = next_pos
+
+    max_total = max(max_total, total)
+print(max_total)
 
