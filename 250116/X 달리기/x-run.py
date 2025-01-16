b90c67683eb0bc32fe56ba341462e96686647a22
buhyun <102472949+buhyun1@users.noreply.github.com>
2025-01-16 22:41:59 +0900

    Title: X 달리기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250116/X 달리기/x-run.py
+++ 250116/X 달리기/x-run.py
@@ -1,26 +1,30 @@
-X = int(input())
+x = int(input())
+t = 0
+left_dist = x
+v = 1
 
-curr_pos = 0
-curr_speed = 1
-sum_time = 0
-while (True):
-    curr_pos += curr_speed
-    sum_time += 1
-    curr_speed += 1
-    increase_sum = 0
+while True:
+    left_dist -= v
+    t += 1
 
-    if (curr_pos == X):
+    if left_dist == 0:
         break
-    
-    for i in range(curr_speed):
-        increase_sum += i
-    if X - (curr_pos + curr_speed) < increase_sum:
-        curr_speed -= 1
-    
-    stay_sum = 0
-    for i in range(curr_speed):
-        stay_sum += i
-    if X - (curr_pos + curr_speed) < stay_sum:
-        curr_speed -= 1
 
-print(sum_time)
+    # 속도를 더 높여도 괜찮은지 결정합니다.
+    # 속도를 1 더 높이면, (v + 1) + v + (v - 1) + ... + 2 + 1 만큼 이동하게 됩니다.
+    # 즉, 남은 거리가 (v + 1) * (v + 2) / 2 보다 크거나 같아야 합니다.
+    if left_dist >= (v + 1) * (v + 2) / 2:
+        v += 1
+    
+    # 속도를 유지해도 괜찮은지 결정합니다.
+    # 속도를 유지하면, v + (v - 1) + ... + 2 + 1 만큼 이동하게 됩니다.
+    # 즉, 남은 거리가 v * (v + 1) / 2 보다 크거나 같아야 합니다.
+    elif left_dist >= v * (v + 1) / 2:
+        # 아무 것도 하지 않습니다.
+        pass
+
+    # 위 둘을 만족하지 못하면 반드시 속도를 줄여야만 합니다.
+    else:
+        v -= 1
+
+print(t)
