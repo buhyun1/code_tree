8065d5f68e0ed4a144b62f3c9e8921d83cea23fc
buhyun <102472949+buhyun1@users.noreply.github.com>
2025-01-16 21:21:04 +0900

    Title: 전부 겹치는 선분 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250116/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 250116/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,17 +1,15 @@
 n = int(input())
 box = [list(map(int, input().split())) for _ in range(n)]
 
-def is_intersecting_after_removal(n, box):
-    for k in range(n):  # k번째 선분을 제거
-        left, right = -1, 101  # 초기값: 최대 범위를 설정
-        for i in range(n):
-            if i == k:  # k번째 선분은 제외
-                continue
-            # 나머지 선분들의 겹치는 구간 갱신
-            left = max(left, box[i][0])  # 최댓값으로 겹치는 왼쪽 구간 갱신
-            right = min(right, box[i][1])  # 최솟값으로 겹치는 오른쪽 구간 갱신
-        if left <= right:  # 공통 구간이 존재
-            return "Yes"
-    return "No"
+check = set()
 
-print(is_intersecting_after_removal(n, box))
+# 각 선분의 범위를 집합에 추가
+for i, v in box:
+    for j in range(i, v + 1):
+        check.add(j)
+
+# 1부터 n까지 모든 값이 포함되어 있는지 확인
+if set(range(1, n + 1)) <= check:  # 부분집합 관계 확인
+    print("Yes")
+else:
+    print("No")
