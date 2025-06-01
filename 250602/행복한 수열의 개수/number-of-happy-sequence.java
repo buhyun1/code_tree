b565a44d8df51d18732ac427a380053c72f63941
buhyun <102472949+buhyun1@users.noreply.github.com>
2025-06-02 08:31:47 +0900

    Title: 행복한 수열의 개수, Time: 95ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250602/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] token = br.readLine().split(" ");
+        int N = Integer.parseInt(token[0]);
+        int M = Integer.parseInt(token[1]);
+
+        int[][] matrix = new int[N][N];
+
+        for (int i=0; i<N; i++){
+            String[] tokens = br.readLine().split(" ");
+            for (int j=0; j<N; j++){
+                matrix[i][j] = Integer.parseInt(tokens[j]);
+            }
+        }
+
+        int happycount = 0;
+
+        for (int i=0; i < N; i++){
+            int count = 1;
+            boolean ishappy = false;
+            for (int j= 1; j < N; j++){
+                if (matrix[i][j] == matrix[i][j-1]){
+                    count += 1;
+                    if (count >= M) {
+                        ishappy = true;
+                        break;
+                    }
+                }else{
+                    count = 1;
+                }
+            }
+            if (ishappy) happycount++;
+        }
+        
+        for (int j=0; j < N; j++){
+            int count = 1;
+            boolean ishappy = false;
+            for (int i= 1; i < N; i++){
+                if (matrix[i][j] == matrix[i-1][j]){
+                    count += 1;
+                    if (count >= M) {
+                        ishappy = true;
+                        break;
+                    }
+                }else{
+                    count = 1;
+                }
+            }
+            if (ishappy) happycount++;
+        }
+        System.out.println(happycount);
+    }
+}
